f5f4e7f1aa695ee1ee8634d6336e64e4fc079b81
Rocio Tarda <rtarda@fi.uba.ar>
2024-11-23 16:08:27 -0300

    Fix Cors error

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    '*'
+    'http://localhost:5173'
 ]
 
 app.add_middleware(
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 # Include routes from the example module
